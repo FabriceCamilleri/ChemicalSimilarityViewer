ccf88de06df0909ccbb51f328ba0c1031c4ea566
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-03-11 13:37:00 +0100

    #7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -42,9 +42,9 @@ function App() {
       }
     )
       .then((response) => response.json())
-      .then(res => { document.getElementById('name').innerHTML = "Nombre de molécules :" + res.file; })
+      .then(res => { document.getElementById('name').innerHTML = res.nb_molecules != -1 ? "Nombre de molécules : " + res.nb_molecules : "Erreur : Mauvais type de fichier"; })
       .catch((error) => {
-        console.error('Error:', error);
+        document.getElementById('name').innerHTML = "Erreur";
       });
   };
 
