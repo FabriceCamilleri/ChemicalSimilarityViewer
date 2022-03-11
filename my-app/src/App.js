01e8b5a7ce9d63c0353edd0fd47f133cc0737e8f
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-03-11 14:14:05 +0100

    oups2 + html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,21 +2,6 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-
-//   return (
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <button onClick={Send}>
-//     //       Send
-//     //     </button>
-//     //   </header>
-//     // </div>
-//     <div>
-//   );
-// }
-
-// export default App;
 
 function App() {
   const [selectedFile, setSelectedFile] = useState();
