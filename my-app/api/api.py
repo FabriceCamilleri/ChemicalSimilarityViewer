9aa7556d260246446381955b017287fbcc55f85c
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-03-11 12:29:51 +0100

    debut pour upload file

--- my-app/api/api.py
+++ my-app/api/api.py
@@ -1,14 +1,48 @@
 import time
 from flask import Flask, request
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', '.csv', '7z'}
 
 app = Flask(__name__)
-
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app = Flask(__name__)
 
 
 @app.route('/post', methods=['POST'])
 def updateCurrentName():
     return request.json
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/file', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    print("req files :", request.files)
+    return {'file': "test.csv"}
+    # if 'file' not in request.files:
+    #     print("test1")
+    #     #flash('No file part')
+    #     # return redirect(request.url)
+    #     return {file: "first"}
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        #flash('No selected file')
+        print("test2")
+        # return redirect(request.url)
+        return {file: "second"}
+    if file and allowed_file(file.filename):
+        print("i saved the file")
+        # filename = secure_filename(file.filename)
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # return redirect(url_for('download_file', name=filename))
+        return {file: "file.csv"}
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,77 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+
+//   return (
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <button onClick={Send}>
+//     //       Send
+//     //     </button>
+//     //   </header>
+//     // </div>
+//     <div>
+//   );
+// }
+
+// export default App;
+
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  var isSelected = false;
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+
+    isSelected = true;
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append('File', selectedFile);
+
+    fetch(
+      '/file',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('Success:', result.file);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={Send}>
-          Send
-        </button>
-      </header>
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {isSelected ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+          <p>
+            lastModifiedDate:{' '}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
     </div>
-  );
-}
+  )
+};
+
 
 export default App;
 
@@ -31,3 +90,5 @@ function Send() {
     .then((response) => response.json()
       .then(res => { document.getElementById('name').innerHTML = "hello " + res.title; }));
 }
+
+
