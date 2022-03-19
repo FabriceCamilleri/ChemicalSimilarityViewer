1bd8a6761ff08ea80cf6e61b15ded87909eb33cb
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-03-19 14:30:00 +0100

    Disabled button and hilightted colum on hovering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,15 +8,23 @@ var selected_column;
 
 function App() {
   const [selectedFile, setSelectedFile] = useState();
+  const [disable, setDisable] = useState(true);
 
   const changeHandler = (event) => {
+    setDisable(true)
     setSelectedFile(event.target.files[0]);
+    let text = document.createTextNode("Select the column containing SMILES code")
+    let strong = document.createElement("strong")
+    let p = document.createElement("p")
+    strong.appendChild(text)
+    p.appendChild(strong)
     let reader = new FileReader();
     reader.readAsText(event.target.files[0]);
     reader.onload = function () {
       document.getElementById("name").innerHTML = "";
+      document.getElementById("name").appendChild(p);
       document.getElementById("name").appendChild(jsonToHTMLTable(csvToJson(reader.result.split('\n').slice(0, 10).join('\n'))));
-      highlight_row()
+      select_column(setDisable)
     }
 
 
@@ -60,11 +68,13 @@ function App() {
       <input className="form-control-file" type="file" name="file" onChange={changeHandler} />
       <br />
       <div>
-        <button className="btn btn-primary" onClick={handleSubmission}>Submit</button>
+        <button id="submitButton" className="btn btn-primary" disabled={disable} onClick={handleSubmission}>Submit</button>
       </div>
     </div>
   )
 };
+
+
 function csvToJson(csv) {
   const lines = csv.split('\n')
   const result = []
@@ -165,23 +175,45 @@ function Send() {
       .then(res => { document.getElementById('name').innerHTML = "hello " + res.title; }));
 }
 
-function highlight_row() {
+function select_column(setDisable) {
   var table = document.getElementById("sentCSV");
   var cells = table.getElementsByTagName("td");
   for (var i = 0; i < cells.length; i++) {
     var cell = cells[i];
+
     cell.onclick = function () {
-      const parentTds = this.parentElement.children;
-      const clickedTdIndex = [...parentTds].findIndex(td => td == this);
-      selected_column = clickedTdIndex;
-      const columns = document.querySelectorAll(`td:nth-child(${clickedTdIndex + 1}), th:nth-child(${clickedTdIndex + 1})`);
-      document.querySelectorAll('.selected').forEach(col => col.classList.remove('selected'));
-      columns.forEach(col => {
-        col.classList.add('selected');
-      });
+      clickEvent(this)
+      setDisable(false)
     }
+
+    cell.onmouseover = function () {
+      mouseEvent(this, true)
+    }
+
+    cell.onmouseleave = function () {
+      mouseEvent(this, false)
+    }
+
   }
 
+}
+
+function mouseEvent(cell, isEntering) {
+  const parentTds = cell.parentElement.children;
+  const clickedTdIndex = [...parentTds].findIndex(td => td == cell);
+  selected_column = clickedTdIndex;
+  const columns = document.querySelectorAll(`td:nth-child(${clickedTdIndex + 1}), th:nth-child(${clickedTdIndex + 1})`);
+  document.querySelectorAll('.selected').forEach(col => col.classList.remove('selected'));
+  if (isEntering) columns.forEach(col => { col.classList.add('selected'); });
+}
+
+function clickEvent(cell) {
+  const parentTds = cell.parentElement.children;
+  const clickedTdIndex = [...parentTds].findIndex(td => td == cell);
+  selected_column = clickedTdIndex;
+  const columns = document.querySelectorAll(`td:nth-child(${clickedTdIndex + 1}), th:nth-child(${clickedTdIndex + 1})`);
+  document.querySelectorAll('.highlighted').forEach(col => col.classList.remove('highlighted'));
+  columns.forEach(col => { col.classList.add('highlighted'); });
 }
 
 function updateQueryStringParameter(uri, key, value) {
