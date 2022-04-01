94cebbd2256a7f4e01b672261eb8904c2cf719d0
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-04-01 15:24:35 +0200

    Selectionner le graph

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ const ALLOWED_FILE = 'csv'
 
 var selected_column;
 var selected_columnName;
+var jsonResult;
 
 function App() {
   const [selectedFile, setSelectedFile] = useState();
@@ -61,6 +62,7 @@ function App() {
     url = updateQueryStringParameter(url, "d1", d1)
     url = updateQueryStringParameter(url, "d2", d2)
     url = updateQueryStringParameter(url, "d3", d3)
+    createMenu([algo1, algo2, d1, d2, d3])
     fetch(
       url,
       {
@@ -108,13 +110,86 @@ function App() {
   )
 };
 
+
+function createMenu(checkList) {
+
+  // let comb = [['X_tsne_DiceDist', 'Y_tsne_DiceDist'], // 1, 0, 1, 0, 0 0
+  // ['X_tsne_CosDist', 'Y_tsne_CosDist'],//1, 0, 0, 1, 0 1
+  // ['X_tsne_TanimotoDist', 'Y_tsne_TanimotoDist'],//1, 0, 0, 0, 1
+  // ['X_umap_DiceDist', 'Y_umap_DiceDist'],// 0, 1, 1, 0, 0
+  // ['X_umap_CosDist', 'Y_umap_CosDist'],// 0, 1, 0, 1, 0
+  // ['X_umap_TanimotoDist', 'Y_umap_TanimotoDist']]// 0, 1, 0, 0, 1
+
+
+
+  //   <label for="algoGr">Choose algo:</label>
+
+  // <select name="algoGr" id="algoGr">
+  //     <option value="0">Dog</option>
+  //     <option value="1">Cat</option>
+  //     <option value="2">Hamster</option>
+  //     <option value="parrot">Parrot</option>
+  //     <option value="spider">Spider</option>
+  //     <option value="goldfish">Goldfish</option>
+  // </select>
+  let selectAlgo = document.createElement('select')
+  selectAlgo.setAttribute('name', 'algoGr')
+  selectAlgo.setAttribute('id', 'algoGr')
+
+  // selectAlgo.setAttribute('onChange', "jsonToGraph(jsonResult)")
+  selectAlgo.onChange = function () { jsonToGraph(jsonResult); };
+
+  let selectDistance = document.createElement('select')
+  selectDistance.setAttribute('name', 'distGr')
+  selectDistance.setAttribute('id', 'distGr')
+  // selectDistance.setAttribute('onChange', "jsonToGraph(jsonResult)")
+
+  selectDistance.onChange = function () { jsonToGraph(jsonResult); };
+
+  let option1 = document.createElement('option')
+  option1.setAttribute('value', 'tsne')
+  option1.innerHTML = "tSNE"
+  let option2 = document.createElement('option')
+  option2.setAttribute('value', 'umap')
+  option2.innerHTML = "UMAP"
+
+
+  let option3 = document.createElement('option')
+  option3.setAttribute('value', 'DiceDist')
+  option3.innerHTML = "Dice"
+  let option4 = document.createElement('option')
+  option4.setAttribute('value', 'CosDist')
+  option4.innerHTML = "Cosine"
+  let option5 = document.createElement('option')
+  option5.setAttribute('value', 'TanimotoDist')
+  option5.innerHTML = "Tanimoto"
+
+  if (checkList[0]) selectAlgo.appendChild(option1)
+  if (checkList[1]) selectAlgo.appendChild(option2)
+  if (checkList[2]) selectDistance.appendChild(option3)
+  if (checkList[3]) selectDistance.appendChild(option4)
+  if (checkList[4]) selectDistance.appendChild(option5)
+
+
+  document.getElementById('graphMenu').innerHTML = ""
+  document.getElementById('graphMenu').appendChild(selectAlgo)
+  document.getElementById('graphMenu').appendChild(selectDistance)
+
+
+}
+
 function jsonToGraph(jsonFile) {
+  jsonResult = jsonFile;
 
   var dataDict = []
   var labelsList = []
+  let algo = document.getElementById("algoGr").value
+  let distance = document.getElementById("distGr").value
 
+  console.log(`X_${algo}_${distance}`);
+  console.log(jsonFile);
   for (var i = 0; i < jsonFile.length; i++) {
-    dataDict.push({ x: parseFloat(jsonFile[i]["X_tsne_DiceDist"]), y: parseFloat(jsonFile[i]["Y_tsne_DiceDist"]) });
+    dataDict.push({ x: parseFloat(jsonFile[i][`X_${algo}_${distance}`]), y: parseFloat(jsonFile[i][`Y_${algo}_${distance}`]) });
     labelsList.push(jsonFile[i]["names"])
   }
 
@@ -144,7 +219,7 @@ function jsonToGraph(jsonFile) {
         },
         title: {
           display: true,
-          text: 'tSNE - Dice distance',
+          text: `${algo} - ${distance}`,
           font: {
             size: 18,
             weight: 'bold',
@@ -216,7 +291,7 @@ function jsonToGraph(jsonFile) {
 
 
 function csvToJson(csv) {
-  const lines = csv.split('\n')
+  const lines = csv.split(/\r\n|\r|\n/)
   const result = []
 
   const headers = lines[0].split(/,|;/)
@@ -275,20 +350,22 @@ function jsonToHTMLTable(json) {
   div.setAttribute('style', 'max-height:570px; max-width:1000px;')
   div.setAttribute('class', 'table-responsive text-nowrap scrollbar-primary')
   div.appendChild(table)
-  var styles = `    
-    .scrollbar-primary::-webkit-scrollbar {
-    width: 17px;
-    background-color: #F5F5F5; }
-    
-    .scrollbar-primary::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-    background-color: #4285F4; }
-    
-    .scrollbar-primary {
-    scrollbar-color: #4285F4 #F5F5F5;
+  var styles = `
+        .scrollbar - primary:: -webkit - scrollbar {
+          width: 17px;
+          background- color: #F5F5F5;
     }
-`
+
+      .scrollbar - primary:: -webkit - scrollbar - thumb {
+        border- radius: 10px;
+    -webkit - box - shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    background - color: #4285F4;
+  }
+    
+    .scrollbar - primary {
+    scrollbar - color: #4285F4 #F5F5F5;
+  }
+  `
 
   var styleSheet = document.createElement("style")
   styleSheet.innerText = styles
