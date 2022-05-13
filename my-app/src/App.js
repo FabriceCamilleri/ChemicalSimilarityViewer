7344263058b03c4f5b58a93b1b436f75debdd17e
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-05-13 16:04:38 +0200

    Code commentaries

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,11 @@ import Chart from 'chart.js/auto';
 // import logo from './logo.svg';
 import './App.css';
 
+//Encodage pris en compte pour le fichier d'entrée
 const ALLOWED_ENCODINGS = ['UTF-8']
-const ALLOWED_FILES = ['csv', 'xlsx', 'xls']
+
+//Extensions du fichiers d'entrée pris en compte
+const ALLOWED_FILES = ['csv', 'xlsx']
 
 var selected_column;
 var selected_columnName;
@@ -17,19 +20,24 @@ var pointIsSelected;
 var colorsOg;
 const languageEncoding = require("detect-file-encoding-and-language");
 
-
+//Application principale
 function App() {
+  //Stocke le fichier soumis par l'utilisateur
   const [selectedFile, setSelectedFile] = useState();
   const [disable, setDisable] = useState(true);
 
   //  The user uploads a file //
 
   const changeHandler = (event) => {
+    //change event appelé dès que l'utilisateur entre un fichier
     var filename = document.getElementById('chooseFile').value;
     var fileSub = event.target.files[0];
 
     let fileUp = document.getElementsByClassName('file-upload')[0]
     let noFile = document.getElementById('noFile')
+
+    //class 'active' pour le css quand fichier valide
+    //class 'wrong' sinon
     if (/^\s*$/.test(filename)) {
       fileUp.classList.remove('active');
       fileUp.classList.remove('wrong');
@@ -39,12 +47,15 @@ function App() {
       fileUp.classList.remove('wrong');
       fileUp.classList.add('active');
       let filena = filename.replace("C:\\fakepath\\", "")
+      //à l'affichage, on troncatène le nom du fichier si trop long
       noFile.innerHTML = filena.length > 28 ? filena.substring(0, 25) + '...' : filena;
     }
 
     if (!fileSub) {
       return
     }
+
+    //on vérifie que l'extension du fichier soit valide, sinon on affiche un message d'avertissement
     if (!ALLOWED_FILES.includes(event.target.files[0].name.split('.')[1])) {
 
       fileUp.classList.remove('active');
@@ -54,6 +65,7 @@ function App() {
       return;
     }
 
+    //on vérifie que l'encodage du fichier soit valide, sinon on affiche un message d'avertissement
     languageEncoding(fileSub).then((fileInfo) => {
       if (!ALLOWED_ENCODINGS.includes(fileInfo.encoding) && fileSub.name.split('.')[1] == "csv") {
         fileUp.classList.remove('active');
@@ -62,12 +74,13 @@ function App() {
         document.getElementById("chartDiv").innerHTML = "The file encoding: " + fileInfo.encoding + " is not correct, please submit an UTF-8-encoded file";
         return;
       }
+      //si le fichier est valide, on set le current selected file
       else {
         setSelectedFile(event.target.files[0]);
         setDisable(true)
+        //on affiche le fichier dans une table
 
-
-
+        //affichage les instructions
         let text = document.createElement("p")
         text.innerHTML = "Select the column containing SMILES code"
         text.setAttribute("id", "selectionText");
@@ -76,12 +89,14 @@ function App() {
         strong.appendChild(text)
         p.appendChild(strong)
 
-
+        //On crée le reader pour parser le fichier
         let reader = new FileReader();
 
+        //2 types d'extensions acceptés : xlsx et csv
 
         //--- xlsx
         if (event.target.files[0].name.split('.')[1] == 'xlsx') {
+          //parsing
           reader.onload = function (e) {
             var data = e.target.result;
             var XLSX = require("xlsx");
@@ -90,19 +105,28 @@ function App() {
             });
             workbook.SheetNames.forEach((sheetName) => {
               var XLSX = require("xlsx");
+              //on convertit le xlsx en json
               let json_object = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+              //ajout du paragraphe d'instruction
               clear(p)
+              //ajout de la table (on convertit le json en table html)
               document.getElementById("chartDiv").appendChild(jsonToHTMLTable(json_object));
+              //les colonnes sont interactibles (on peut les sélectionner)
               select_column(setDisable)
+              //puis conversion du json en csv (comme le back-end s'attendra à recevoir un csv et non pas un xlsx)
               let converter = require('json-2-csv');
               converter.json2csv(json_object, (err, csv) => {
+                //blob de type csv
                 const parts = [
                   new Blob(["\ufeff", csv])
                 ];
+                //création du fichier avec son contenu
                 const csvFile = new File(parts, 'result.csv', {
                   lastModified: Date.now(),
                   type: "text/csv"
                 });
+                //reset le fichier selectionné par l'utilisateur par celui nouvellement crée
+                //au moment du submit, on enverra le fichier actuellement sélectionné
                 setSelectedFile(csvFile);
               })
             });
@@ -122,6 +146,7 @@ function App() {
           reader.readAsText(event.target.files[0]);
           reader.onload = function () {
             clear(p)
+            //on convertit le csv en json, pour ensuite convertir en table html et l'ajouter à la page
             document.getElementById("chartDiv").appendChild(jsonToHTMLTable(csvToJson(reader.result.split('\n').slice(0, 10).join('\n'))));
             select_column(setDisable)
           }
@@ -133,11 +158,12 @@ function App() {
   //  The user sends the request by clicking on the submit button //
 
   const handleSubmission = () => {
+    //vérification de la validité du fichier
     if (!selectedFile || !ALLOWED_FILES.includes(selectedFile.name.split('.')[1])) return
     const formData = new FormData();
 
     formData.append('File', selectedFile);
-
+    //la sélection des colonnes ainsi que le choix des algos/distances sont paramètrisés dans l'url
     var url = updateQueryStringParameter("/file", "index", selected_column)
     url = updateQueryStringParameter(url, "nameIndex", selected_columnName)
     var algo1 = + document.getElementById("algo1").checked
@@ -147,6 +173,8 @@ function App() {
     var d1 = + document.getElementById("d1").checked
     var d2 = + document.getElementById("d2").checked
     var d3 = + document.getElementById("d3").checked
+
+    //si l'utilisateur n'a coché aucune algo/distance, on affiche un message d'avertissement
     if (!(algo1 | algo2)) {
       document.getElementById("chartDiv").innerHTML = "Please choose an algorithm.";
       return;
@@ -158,7 +186,11 @@ function App() {
     url = updateQueryStringParameter(url, "d1", d1)
     url = updateQueryStringParameter(url, "d2", d2)
     url = updateQueryStringParameter(url, "d3", d3)
+
+    //création du menu déroulant : il pourra ainsi afficher le graphe en fonction des différentes distances/algos qui a sélectionné
     createMenu([algo1, algo2, d1, d2, d3])
+
+    //requête post pour envoyer le fichier au back-end avec l'url paramétrisé
     fetch(
       url,
       {
@@ -197,6 +229,7 @@ function App() {
 
   document.getElementById('chooseFile').addEventListener('change', (e) => changeHandler(e))
 
+  //construction du boutton submit en React
   return (
     <div style={{ textAlign: "center" }}>
       <button id="submitButton" className="btn btn-primary" disabled={disable} onClick={handleSubmission}>Submit</button>
@@ -204,11 +237,12 @@ function App() {
   )
 };
 
-
+//construit le menu déroulant à partir du tableau de boolean
 function createMenu(checkList) {
   let selectAlgo = document.createElement('select')
   selectAlgo.setAttribute('name', 'algoGr')
   selectAlgo.setAttribute('id', 'algoGr')
+  //au changement, on retire le graphique actuellement et on le remplace par celui qui correspond aux options choisis
   selectAlgo.addEventListener('change', () => {
     myChart.destroy();
     jsonToGraph(jsonResult);
@@ -222,6 +256,8 @@ function createMenu(checkList) {
     jsonToGraph(jsonResult);
   })
 
+  //création de toute les options
+  //--- algorithmes
   let option1 = document.createElement('option')
   option1.setAttribute('value', 'tsne')
   option1.innerHTML = "tSNE"
@@ -229,7 +265,7 @@ function createMenu(checkList) {
   option2.setAttribute('value', 'umap')
   option2.innerHTML = "UMAP"
 
-
+  //--- distances
   let option3 = document.createElement('option')
   option3.setAttribute('value', 'DiceDist')
   option3.innerHTML = "Dice"
@@ -240,12 +276,14 @@ function createMenu(checkList) {
   option5.setAttribute('value', 'TanimotoDist')
   option5.innerHTML = "Tanimoto"
 
+  //on l'ajoute au menu déroulant que s'il le faut, en fonction de la liste des booléens
   if (checkList[0]) selectAlgo.appendChild(option1)
   if (checkList[1]) selectAlgo.appendChild(option2)
   if (checkList[2]) selectDistance.appendChild(option3)
   if (checkList[3]) selectDistance.appendChild(option4)
   if (checkList[4]) selectDistance.appendChild(option5)
 
+  //ajout des labels + du padding
   let algoLabel = document.createElement('label')
   algoLabel.setAttribute('style', 'padding-right: 5px')
   algoLabel.innerHTML = "Algorithm"
@@ -256,6 +294,7 @@ function createMenu(checkList) {
   distanceLabel.setAttribute('style', 'padding-right: 5px')
   distanceLabel.innerHTML = "Distance"
 
+  //ajout du menu crée à la page
   let graphMenu = document.getElementById('graphMenu')
   graphMenu.innerHTML = ""
   graphMenu.appendChild(algoLabel)
@@ -432,11 +471,14 @@ function highLightGraph(pointToChange) {
 //  Transforms a csv file to Json //
 
 function csvToJson(csv) {
+  //split sur les lignes
   const lines = csv.split(/\r\n|\r|\n/)
+
+  //json
   const result = []
 
   const headers = lines[0].split(/,|;/)
-
+  //boucle sur les lignes
   for (let i = 1; i < lines.length; i++) {
     if (!lines[i])
       continue
@@ -454,6 +496,7 @@ function csvToJson(csv) {
 //  Transforms a JSON file to an HTML table //
 
 function jsonToHTMLTable(json) {
+  //itération sur le attributs pour avoir les colonnes
   var col = [];
   for (var i = 0; i < json.length; i++) {
     for (var key in json[i]) {
@@ -462,7 +505,7 @@ function jsonToHTMLTable(json) {
       }
     }
   }
-
+  //initialise la table html
   var table = document.createElement("table");
 
 
