d360368e5dd92550083abe1256f2f2111ccff359
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-05-18 10:49:05 +0200

    #32 Commentaries back-end

--- my-app/api/api.py
+++ my-app/api/api.py
@@ -32,29 +32,54 @@ def updateCurrentName():
 
 
 def allowed_file(filename):
+    """The filename is accepted or not
+
+    Parameters
+    ----------
+    filename : str
+        The name of the file
+
+    Returns
+    -------
+    boolean
+        The extension belongs the allowed extensions
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/api/file', methods=['POST'])
 def upload_file():
+    """The main endpoint. The user has submitted a file and the back-end will process the calculation.
+
+    Returns
+    -------
+    dict
+        a json, containing the key which identify the work, in response to the front-end
+    """
+    # content's name must be "File"
     if 'File' not in request.files:
         print("if1")
         return {'nb_molecules': -1}
 
     file = request.files['File']
     print(file.filename)
+    # file must be valid
     if file.filename == '' or file and not allowed_file(file.filename):
         print("if2")
         return {'nb_molecules': -1}
 
+    # user choices is retrieved in the url
     index = request.args.get('index')
     indexName = request.args.get('nameIndex')
+    # currently, 2 algos can be chosen
     listAlgo = [int(request.args.get('algo1')), int(request.args.get('algo2'))]
+    # currently, 3 types of distances can be chosen
     listDist = [int(request.args.get('d1')), int(
         request.args.get('d2')), int(request.args.get('d3'))]
     # "[true, false]
     print("file=", file)
+    # Reading of the csv's content
     df = pd.read_csv(request.files['File'], sep=";|,", header=None)
     print("df=", df)
     df = chemspace.createChemicalSpace(
